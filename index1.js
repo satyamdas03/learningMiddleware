2e7f8162a8ccb7c5672ac74e6d19bac6cc12d9ae
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-22 15:20:16 +0530

    something

--- index1.js
+++ index1.js
@@ -7,11 +7,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); //middleware
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
+
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
