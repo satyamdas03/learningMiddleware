19bf9abdc507e5ac53206977e04acf3acb673062
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-22 15:23:11 +0530

    g

--- index2.js
+++ index2.js
@@ -10,3 +10,11 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+//steps to use middleware for logging purposes
+//use npm to install morgan package
+//run index2.js with nodemon
+//import morgan module
+//mount middleware using the Express .use() method
+//test the logging on the localhost and with postman
