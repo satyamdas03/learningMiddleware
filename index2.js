6ff4888619df6c02fa6feedbf0459909c3de9d14
satyamdas03 <116027621+satyamdas03@users.noreply.github.com>
2024-06-22 15:31:47 +0530

    t

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@ import morgan from "morgan";
 
 const app = express();
 const port = 3000;
-app.use(morgan("combined"));
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello");
--- index3.js
+++ index3.js
@@ -12,3 +12,12 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+//DIY MIDDLEWARE(STEPS)
+//run index3.js with nodemon
+//create own function called logger(req, res, next)
+//inside the function console.log() the req method and the req URL.
+//remember to call next()
+//mount your middleware function using the Express .use() method
+//test the logging on the localhost and postman
